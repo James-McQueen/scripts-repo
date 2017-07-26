60c39ebd885a6c465bc2a91d68a9c5d28049c134
James-McQueen <james@mcqueen.org.uk>
2017-07-26 16:43:33 +0100

    Cleaned scripts

--- cloudant_replication/databaseReplicate.py
+++ cloudant_replication/databaseReplicate.py
@@ -1,6 +1,5 @@
 ###26/07/17###
 ###Use: Pass cloudant username, password, source databse name, target database name and create target into replicateDatabase.sh and run.
-
 from cloudant.client import Cloudant
 from cloudant.error import CloudantException
 from cloudant.result import Result, ResultByKey
@@ -10,36 +9,71 @@ from random import randint
 from time import sleep
 import sys
 
-username = sys.argv[1]
-password = sys.argv[2]
-sourceBase = sys.argv[3]
-targetBase = sys.argv[4]
-replId = sys.argv[5]
+def failureCheck(error):
+    if error == 1:              #Check if something went wrong
+        sys.exit(1)
 
-createTarget = sys.argv[6]
-if (str(createTarget) == "false") or (str(createTarget) == "False"):
-    createTarget = False
-else:
-    createTarget = True
+def connect(user, passw, url):
+    global client
+    client = Cloudant(user, passw, url=url)
+    try:
+        client.connect()            #connect to cloudant account
+    except:
+        print("Could not connect to Cloudant. Check credentials")
+        return 1
+
+def checkCreateTarget(createTarget):
+    if (str(createTarget) == "false") or (str(createTarget) == "False"):
+        createTarget = False
+    elif (str(createTarget) == "true") or (str(createTarget) == "True"):
+        createTarget = True
+    else:
+        print("Invalid createTarget")
+        return 1
+
+def checkReplication(replId):
+    for doc in replicatorObject.follow_replication(replId):        #check if replication happened
+        sleep(0.2) #seconds
+        try:
+            if (str(doc['_replication_state']) == 'completed'):
+                print ("Done.")
+            elif (str(doc['_replication_state']) == 'error'):
+                print("Error in replication. Did not replicate. Reason: " + str(doc['_replication_state_reason']))
+                return 1
+        except:
+            print(doc)
+
+def getCommandLineParameters():
+    try:
+        global username, password, sourceBase, targetBase, replId, createTarget
+        username = sys.argv[1]
+        password = sys.argv[2]
+        sourceBase = sys.argv[3]
+        targetBase = sys.argv[4]                    #Get necessary values from shell script
+        replId = sys.argv[5]
+        createTarget = sys.argv[6]
+    except IndexError:
+        print("One or more parameters not specified")
+        return 1
+
+
+failureCheck(getCommandLineParameters())
 
 host = username + ".cloudant.com"
 baseURL = "https://" + username + ":" + password + "@" + host
-
-
-client = Cloudant(username, password, url=baseURL)
-client.connect()                                    #connect to cloudant account
-
-replBase = client["_replicator"]                    #connect to _replicator database
-
-print(replId)
-
 source = "https://" + username + ":" + password + "@" + host + "/" + sourceBase
 target = "https://" + username + ":" + password + "@" + host + "/" + targetBase           #define source and target dataases
+
+failureCheck(checkCreateTarget(createTarget)) #Check create_target was correct
+
+failureCheck(connect(username, password, baseURL)) # Connect to Cloudant
+
+replBase = client["_replicator"]                    #connect to _replicator database
 
 replicationDocument = {
   "source" : source,
   "target" : target,                               #create replication document
-  "create_target": createTarget,
+  "create_target": bool(createTarget),
   "_id" : replId
 }
 
@@ -47,10 +81,4 @@ repldoc = replBase.create_document(replicationDocument)        #Post replication
 
 replicatorObject = Replicator(client)                          #create replication instance
 
-for doc in replicatorObject.follow_replication(replId):        #check if replication happened
-    sleep(0.1)
-    try:
-        if (str(doc['_replication_state']) == 'completed'):
-            print ("done")
-    except:
-        print("Waiting")
+failureCheck(checkReplication(replId))
